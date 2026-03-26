0daf35fb8c02c37d469dc4128ca0ca11d7acdf13
Fredrik.August.Borse.Haraldsen <Fredrik.August.Borse.Haraldsen@nav.no>
2026-03-26 09:02:12 +0100

    create initial spring boot application for nais deployment
    There was no existing repository for the GCP api application.
    
    Added initial spring boot application setup.
    
    Signed-off-by: Borse <borse.schumacher.haraldsen@nav.no>

---
+++ src/main/java/no/nav/oebs/kontoplan_api/KontoplanApiApplication.java
@@ -0,0 +1,13 @@
+package no.nav.oebs.kontoplan_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KontoplanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KontoplanApiApplication.class, args);
+	}
+
+}
