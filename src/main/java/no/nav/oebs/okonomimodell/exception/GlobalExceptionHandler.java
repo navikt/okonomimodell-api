2f3f06a355566059905232fa787f98eaebed7157
Agnes Oftung Strandbu <agnes.oftung.strandbu@nav.no>
2026-04-27 15:49:05 +0200

    fix(OEBS-2051): add handling of JwtToken exceptions

--- src/main/java/no/nav/oebs/okonomimodell/exception/GlobalExceptionHandler.java
+++ src/main/java/no/nav/oebs/okonomimodell/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package no.nav.oebs.okonomimodell.exception;
 
+import no.nav.security.token.support.core.exceptions.JwtTokenMissingException;
+import no.nav.security.token.support.spring.validation.interceptor.JwtTokenUnauthorizedException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -20,6 +22,28 @@ public class GlobalExceptionHandler {
         respons.put("status", 500);
         respons.put("timestamp", LocalDateTime.now());
         return new ResponseEntity<>(respons, org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Map<String, Object>> handleJwtTokenMissingException(
+            JwtTokenMissingException ex) {
+        Map<String, Object> respons = new HashMap<>();
+        respons.put("error", "Missing token to access endpoint");
+        respons.put("message", ex.getMessage());
+        respons.put("status", 401);
+        respons.put("timestamp", LocalDateTime.now());
+        return new ResponseEntity<>(respons, org.springframework.http.HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Map<String, Object>> handleJwtTokenUnauthorizedException(
+            JwtTokenUnauthorizedException ex) {
+        Map<String, Object> respons = new HashMap<>();
+        respons.put("error", "Unauthorized");
+        respons.put("message", ex.getMessage());
+        respons.put("status", 401);
+        respons.put("timestamp", LocalDateTime.now());
+        return new ResponseEntity<>(respons, org.springframework.http.HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler
