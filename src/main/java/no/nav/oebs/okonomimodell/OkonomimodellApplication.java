1f0d3961f5992951cab8e968e12e3f5dc5ed50fc
Agnes Oftung Strandbu <agnes.oftung.strandbu@nav.no>
2026-05-05 14:45:08 +0200

    feat(OEBS-2051): bearer token validation in swagger

--- src/main/java/no/nav/oebs/okonomimodell/OkonomimodellApplication.java
+++ src/main/java/no/nav/oebs/okonomimodell/OkonomimodellApplication.java
@@ -1,11 +1,14 @@
 package no.nav.oebs.okonomimodell;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
 
 @SpringBootApplication
 @EnableJwtTokenValidation(ignore = {"org.springdoc", "org.springframework"})
+@SecurityScheme(type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT", name = "BearerAuth")
 public class OkonomimodellApplication {
 
 	public static void main(String[] args) {
