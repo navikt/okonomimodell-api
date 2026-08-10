26c48021246a81302f6bee18871979a6754ca302
agnesstrandbu-nav <agnes.oftung.strandbu@nav.no>
2026-08-10 10:18:01 +0200

    cleanup(OEBS-2251): remove unnecessary org_id parameter (#101)

--- src/main/java/no/nav/oebs/okonomimodell/controller/ValidateKontostrengController.java
+++ src/main/java/no/nav/oebs/okonomimodell/controller/ValidateKontostrengController.java
@@ -2,7 +2,7 @@ package no.nav.oebs.okonomimodell.controller;
 
 import lombok.AllArgsConstructor;
 import no.nav.oebs.okonomimodell.service.OkonomimodellService;
-import no.nav.security.token.support.core.api.Unprotected;
+import no.nav.security.token.support.core.api.Protected;
 import org.jspecify.annotations.Nullable;
 import org.openapitools.api.ValidatekontostrengApi;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +15,7 @@ public class ValidateKontostrengController implements ValidatekontostrengApi {
     private final OkonomimodellService okonomimodellService;
 
     @Override
-    @Unprotected
+    @Protected
     public ResponseEntity<Boolean> validateKontostreng(@Nullable String artskonto, @Nullable String kostnadssted, @Nullable String produkt, @Nullable String oppgave, @Nullable String felles, @Nullable String statskonto, @Nullable String kilde, @Nullable String tilsagnsaar, @Nullable String frittfelt1, @Nullable String frittfelt2, @Nullable String fullmaktskode, @Nullable String regnskapsforer) {
         return ResponseEntity.ok(okonomimodellService.getKontostrengValidation(artskonto, kostnadssted, produkt, oppgave, felles, statskonto, kilde, tilsagnsaar, frittfelt1, frittfelt2, fullmaktskode, regnskapsforer));
     }
